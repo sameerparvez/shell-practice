ca01f89b6e6135cd13a0a442c9a6929639358757
sameer <samparvez.shaik@gmail.com>
2025-07-12 18:25:01 +0530

    Ansible-roboshop

--- Ansible/Ansible-roboshop/roboshop.sh
+++ Ansible/Ansible-roboshop/roboshop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-01bc7ebe005fb1cb2" # replace with your SG ID
+SG_ID="sg-09910ba92aaf8a435" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch")
-ZONE_ID="Z032558618100M4EJX8X4" # replace with your ZONE ID
-DOMAIN_NAME="daws84s.site" # replace with your domain
+ZONE_ID="Z0024432DFPWVB7GYVA9" # replace with your ZONE ID
+DOMAIN_NAME="sameerdevops.space" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
 for instance in $@
